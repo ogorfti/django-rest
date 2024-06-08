c6861f8762acd5d43211d8641e10696303d54b00
ogorfti <ww.shanks1@gmail.com>
2024-06-08 16:56:55 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,26 @@
+## Django for APIs Project Repository
+
+This repository contains learning projects based on the book "Django for APIs" by William S. Vincent. It includes a Todo API and a Blog API, demonstrating practical applications of Django REST Framework, User Authentication, Viewsets, Routers, Schemas, and Documentation.
+
+
+### Todo API
+The Todo API project is a full-stack application built with Django, Django REST Framework, and React. It covers topics such as models, serializers, views, and API consumption using Axios in the React frontend.
+
+<p float="left">
+  <img src="images/todo-list.png" width="44%" />
+  <img src="images/todo-react.png" width="50%" /> 
+</p>
+
+### Blog API
+
+The Blog API project is a comprehensive demonstration of various advanced Django REST Framework concepts. It starts with the initial setup of the API, creation of models, and writing tests. It then delves into the creation of Django REST Framework URLs, serializers, and views. The project also covers the use of the browsable API.
+
+The subsequent chapters explore more advanced topics such as permissions, user authentication, viewsets, routers, schemas, and documentation. Each topic is explained with practical examples and visualized using Swagger UI.
+
+<p float="left">
+  <img src="images/swagger.png" width="48%" />
+  <img src="images/drf-auth.png" width="48%" /> 
+</p>
+
+### Resources:
+- [Django for APIs Book](https://djangoforapis.com/)
--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -2,32 +2,34 @@ from .serializers import PostSerializer, UserSerializer
 from .models import Post
 from rest_framework import generics, permissions, viewsets
 from .permissions import testSec
-from django.contrib.auth import get_user_model
+# from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 
-# class PostList(generics.ListCreateAPIView):
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
 
-# class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-#     permission_classes = [testSec, ]
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
-# class UserList(generics.ListCreateAPIView):
-#     queryset = get_user_model().objects.all()
-#     serializer_class = UserSerializer
-
-# class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = get_user_model().objects.all()
-#     serializer_class = UserSerializer
-
-# 2nd method
-class PostViewSet(viewsets.ModelViewSet):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [testSec, ]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = get_user_model().objects.all()
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# 2nd method
+# class PostViewSet(viewsets.ModelViewSet):
+#     permission_classes = [testSec, ]
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+# class UserViewSet(viewsets.ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
